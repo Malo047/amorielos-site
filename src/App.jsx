e9d3eb85342de337a928f898773c1b158ab1c7ab
Marlon <marlonhoffmann001@gmail.com>
2025-06-05 18:14:52 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css'
 import { About } from './components/about/About'
-import { Contact } from './components/contact/Contact'
 import { Fashion } from './components/fashion/Fashion'
 import { Header } from "./components/header/Header"
 import { Intro } from './components/intro/Intro'
+import{ Contact } from "./components/contact/Contact"
 
 function App() {
   return (
     <>
-
+      <Header></Header>
       <Intro></Intro>
-
+      <Fashion></Fashion>
+      <About></About>
+      <Contact></Contact>
     </>
   )
 }
